85af04e0f60cb7ab3a4186c9959b8f903278c672
iColdPlayer <slashsdull@gmail.com>
2019-01-11 07:43:55 +0700

    SECRET_KEY Updated

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'embp+xdoj&ipyudk-w**o76eg-xqvv2#ppb0^m9nd6=#vxx9q2'
+SECRET_KEY = '{{SECRET_KEY}}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
